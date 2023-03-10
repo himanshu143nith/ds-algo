6b2fe2f919c8879f62a025152d6372b250f4eedc
Himanshu Sharma <himanshu143nith@users.noreply.github.com>
2023-03-10 13:41:55 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,16 @@
-print("Pyton application for ds algo")
+
+class Calculator(object):
+    def __init__(self, *args):
+        super(Calculator, self).__init__(*args)
+
+    def addition(self, a, b):
+        return a + b
+
+    def substraction(self, a, b):
+        return a - b
+
+    def multiplication(self, a, b):
+        return a * b
+
+    def division(self, a, b):
+        return a / b
